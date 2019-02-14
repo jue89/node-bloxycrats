eb1856eca11f0ebb2a2c104942d1b4bb7769cecf
Jue <me@jue.yt>
2019-02-15 00:36:59 +0100

    Added stream processor

---
+++ __tests__/index.js
@@ -0,0 +1,93 @@
+const events = require('events');
+
+const mockStreamFactory = () => {
+	const e = new events.EventEmitter();
+	e.write = jest.fn();
+	return e;
+};
+
+const Bloxy = require('../index.js');
+
+test('send buffer', () => {
+	const stream = mockStreamFactory();
+	const block = Buffer.alloc(64, 'a');
+	const len = Buffer.alloc(4);
+	len.writeUInt32BE(block.length, 0);
+	const b = new Bloxy(stream);
+	b.send(block);
+	expect(stream.write.mock.calls[0][0].toString('hex')).toEqual(len.toString('hex'));
+	expect(stream.write.mock.calls[1][0].toString('hex')).toEqual(block.toString('hex'));
+});
+
+test('send an array of buffer', () => {
+	const stream = mockStreamFactory();
+	const block = Buffer.alloc(64, 'a');
+	const len = Buffer.alloc(4);
+	len.writeUInt32BE(2 * block.length, 0);
+	const b = new Bloxy(stream);
+	b.send([block, block]);
+	expect(stream.write.mock.calls[0][0].toString('hex')).toEqual(len.toString('hex'));
+	expect(stream.write.mock.calls[1][0].toString('hex')).toEqual(block.toString('hex'));
+	expect(stream.write.mock.calls[2][0].toString('hex')).toEqual(block.toString('hex'));
+});
+
+test('receive buffer', () => {
+	const stream = mockStreamFactory();
+	const block = Buffer.alloc(64, 'a');
+	const data = Buffer.concat([Buffer.alloc(4), block]);
+	data.writeUInt32BE(block.length, 0);
+	const b = new Bloxy(stream);
+	const onMessage = jest.fn();
+	b.on('message', onMessage);
+	stream.emit('data', data);
+	expect(onMessage.mock.calls[0][0].toString('hex')).toEqual(block.toString('hex'));
+});
+
+test('receive splitted buffer', () => {
+	const stream = mockStreamFactory();
+	const block = Buffer.alloc(64, 'a');
+	const data = Buffer.concat([Buffer.alloc(4), block]);
+	data.writeUInt32BE(block.length, 0);
+	const b = new Bloxy(stream);
+	const onMessage = jest.fn();
+	b.on('message', onMessage);
+	stream.emit('data', data.slice(0, 8));
+	stream.emit('data', data.slice(8));
+	expect(onMessage.mock.calls[0][0].toString('hex')).toEqual(block.toString('hex'));
+});
+
+test('receive buffer byte by byte', () => {
+	const stream = mockStreamFactory();
+	const block = Buffer.alloc(64, 'a');
+	const data = Buffer.concat([ Buffer.alloc(4), block ]);
+	data.writeUInt32BE(block.length, 0);
+	const b = new Bloxy(stream);
+	const onMessage = jest.fn();
+	b.on('message', onMessage);
+	for (let i = 0; i < data.length; i++) {
+		stream.emit('data', data.slice(i, i + 1));
+	}
+	expect(onMessage.mock.calls[0][0].toString('hex')).toEqual(block.toString('hex'));
+});
+
+test('receive two buffers in one chunk', () => {
+	const stream = mockStreamFactory();
+	const block = Buffer.alloc(64, 'a');
+	const data = Buffer.concat([ Buffer.alloc(4), block ]);
+	data.writeUInt32BE(block.length, 0);
+	const b = new Bloxy(stream);
+	const onMessage = jest.fn();
+	b.on('message', onMessage);
+	stream.emit('data', Buffer.concat([data, data]));
+	expect(onMessage.mock.calls[0][0].toString('hex')).toEqual(block.toString('hex'));
+	expect(onMessage.mock.calls[1][0].toString('hex')).toEqual(block.toString('hex'));
+});
+
+test('forward close event', () => {
+	const stream = mockStreamFactory();
+	const b = new Bloxy(stream);
+	const onClose = jest.fn();
+	b.on('close', onClose);
+	stream.emit('close');
+	expect(onClose.mock.calls.length).toBe(1);
+});
---
+++ index.js
@@ -0,0 +1,53 @@
+const events = require('events');
+
+class Bloxy extends events.EventEmitter {
+	constructor (stream) {
+		super();
+		this.stream = stream;
+
+		// Setup stream processor
+		let chunks = [];
+		let chunksLength = 0;
+		let length = -1;
+		const squashChunks = () => { chunks = [ Buffer.concat(chunks) ]; };
+		this.stream.on('data', (chunk) => {
+			chunks.push(chunk);
+			chunksLength += chunk.length;
+			while (true) {
+				if (length === -1 && chunksLength >= 4) {
+					// No length has been read, yet -> try to read it
+					squashChunks();
+					length = chunks[0].readUInt32BE(0);
+				} else if (length !== -1 && chunksLength >= length + 4) {
+					// We received enough bytes for the current block
+					squashChunks();
+					this.emit('message', chunks[0].slice(4, length + 4));
+					chunksLength -= length + 4;
+					if (chunksLength > 0) {
+						chunks[0] = chunks[0].slice(length + 4);
+					} else {
+						chunks = [];
+					}
+					length = -1;
+				} else {
+					break;
+				}
+			}
+		});
+
+		// Forward close events
+		this.stream.on('close', () => this.emit('close'));
+	}
+
+	send (buf) {
+		if (!(buf instanceof Array)) buf = [ buf ];
+		const lengthField = Buffer.alloc(4);
+		lengthField.writeUInt32BE(buf.reduce((len, b) => len + b.length, 0), 0);
+		this.stream.write(lengthField);
+		return Promise.all(buf.map((b) => new Promise((resolve) => {
+			this.stream.write(b, resolve);
+		})));
+	}
+}
+
+module.exports = Bloxy;
